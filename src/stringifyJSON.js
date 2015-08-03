45fbf5fe8f01bf6cf23c7e6518a16104e38e64ef
Brett Sanders <bsand08@gmail.com>
2015-08-02 18:55:09 -0700

    Implemented stringifyJSON function.

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,38 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (obj === null) {
+  	return "null";
+  } else if (obj === true) {
+  	return "true";
+  } else if (obj === false) {
+  	return "false"
+  } else if (typeof obj === "number") {
+  	return "" + obj;
+  } else if (typeof obj === "string") {
+  	return "\"" + obj + "\"";
+  } else if (Array.isArray(obj)) {
+  	var result = "["
+	for (var i = 0; i < obj.length; i++) {
+	  if (i === obj.length - 1) {
+	    result = result.concat(stringifyJSON(obj[i]));
+	  } else {
+	  	result = result.concat(stringifyJSON(obj[i])) + ",";
+	  }
+	 }
+	 result = result.concat("]");
+	 return result;
+  } else if (typeof obj === "object") {
+  	var result = "{";
+	for(var keys = Object.keys(obj), i = 0, end = keys.length; i < end; i++) {
+		if (typeof obj[keys[i]] !== "function" && obj[keys[i]] !== undefined) {
+	  		result = result.concat(stringifyJSON(keys[i]) + ":" + stringifyJSON(obj[keys[i]]));
+	  		if (i !== end - 1) {
+	  			result = result.concat(",");
+	  		}
+	  	}
+  	}
+  	result = result.concat("}");
+  	return result;
+  }
 };
